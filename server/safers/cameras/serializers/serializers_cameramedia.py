7ff64366cbda326e514fdf71ddf94159253c211b
Allyn Treshansky <allyn.treshansky@gmail.com>
2023-05-18 15:47:31 +0100

    chore(backend): linting
    Updated the python interpreter locally to ensure linting works in
    vscode.

--- server/safers/cameras/serializers/serializers_cameramedia.py
+++ server/safers/cameras/serializers/serializers_cameramedia.py
@@ -52,4 +52,6 @@ class CameraMediaSerializer(serializers.ModelSerializer):
     #     precision=CameraMedia.PRECISION, allow_null=True, required=False
     # )
 
-    favorite = serializers.BooleanField(read_only=True, required=False, allow_null=True,)
+    favorite = serializers.BooleanField(
+        read_only=True, required=False, allow_null=True
+    )
